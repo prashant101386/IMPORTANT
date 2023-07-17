611b702c764e0de40ed40cc4fe779af2230b227c
Prashant Singh <prashant.101386@gmail.com>
2023-07-17 16:40:26 +0530

    removed hardcoding

--- deployment/modules/prj/cluster.ps1
+++ deployment/modules/prj/cluster.ps1
@@ -1,5 +1,5 @@
 $baseUrl = "https://adb-4656259455953667.7.azuredatabricks.net/api/2.0"
-$patToken = "Bearer dapib8de9421c17943bc3f26a219c7ccb443"
+$patToken = $env:PAT
 
 $headers = @{
     "Authorization" = $patToken
