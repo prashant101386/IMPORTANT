efb14b7b699af636b451ec2183ac284b9df7b4ef
Prashant Singh <prashant.101386@gmail.com>
2023-07-17 20:14:06 +0530

    Try

--- deployment/modules/prj/cluster.ps1
+++ deployment/modules/prj/cluster.ps1
@@ -1,7 +1,7 @@
 param([string] $token)
 
 $baseUrl = "https://adb-4656259455953667.7.azuredatabricks.net/api/2.0"
-$patToken = "Bearer ${token}" 
+$patToken = "${token}" 
 
 $headers = @{
     "Authorization" = $patToken
