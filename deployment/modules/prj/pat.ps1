c4dccbd5112c4d905e7a8062467aa428c6ea86b8
Prashant Singh <prashant.101386@gmail.com>
2023-08-03 01:07:20 +0530

    Changed db instance

--- deployment/modules/prj/pat.ps1
+++ deployment/modules/prj/pat.ps1
@@ -1,7 +1,7 @@
 param([string] $token)
 
 # Replace these variables with your Databricks workspace details and access token
-$workspaceBaseUrl = "https://adb-4892209284065921.1.azuredatabricks.net"
+$workspaceBaseUrl = "https://adb-1919850944000482.2.azuredatabricks.net"
 $patToken = $token
 # Set the token duration in seconds (e.g., 30 days = 2592000 seconds)
 $tokenLifetimeSeconds = 2592000
