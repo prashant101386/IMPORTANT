272acc52bc56b53144ad3d78ef72cefdd3ad7065
Prashant Singh <prashant.101386@gmail.com>
2023-08-02 17:17:36 +0530

    trial

--- deployment/modules/prj/acl.ps1
+++ deployment/modules/prj/acl.ps1
@@ -3,7 +3,7 @@ $SaKey = "${keys}"
 $filesystemName = "${Env:fsname}"
 $userId = "${Env:user}"
 
-$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $SaKey
+$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey ${SaKey}
 $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $userId -Permission r--
 Update-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName -Acl $acl
 $filesystem = Get-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName
