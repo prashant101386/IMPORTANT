d0b83243d1899c2cad43ff91898182362a7abb85
Prashant Singh <prashant.101386@gmail.com>
2023-08-02 17:43:13 +0530

    Single quotes

--- deployment/modules/prj/acl.ps1
+++ deployment/modules/prj/acl.ps1
@@ -2,7 +2,7 @@ $storageAccountName = "${Env:saname}"
 $filesystemName = "${Env:fsname}"
 $userId = "${Env:user}"
 $keys
-$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey "${keys}"
+$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey '${keys}'
 $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $userId -Permission r--
 Update-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName -Acl $acl
 $filesystem = Get-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName
