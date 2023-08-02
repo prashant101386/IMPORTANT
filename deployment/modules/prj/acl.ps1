29ee37e47066a9e2b3323748fca70979a1f367eb
Prashant Singh <prashant.101386@gmail.com>
2023-08-02 16:04:53 +0530

    Different method

--- deployment/modules/prj/acl.ps1
+++ deployment/modules/prj/acl.ps1
@@ -2,8 +2,9 @@ $storageAccountName = "${Env:saname}"
 $StorageAccountKey = "${keys}"
 $filesystemName = "${Env:fsname}"
 $userId = "${Env:user}"
+$secret = Get-AzKeyVaultSecret -VaultName "dmw2dihadbkv01-learning" -Name "SAKey" -AsPlainText
 
-$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey 'DsAWseTsUqtVx+zrdm/AylHJkFxK+hjq7etYoBg5skV/N3C69nh8X4DT+qlWlu3xv5tByc2CvUjB+AStKYkVLA=='
+$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $secret
 $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $userId -Permission r--
 Update-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName -Acl $acl
 $filesystem = Get-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName
