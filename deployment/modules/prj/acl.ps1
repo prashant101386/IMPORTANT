76cc3b8bc7217755beb5cc8aceab6a6bf08ece48
Prashant Singh <prashant.101386@gmail.com>
2023-08-03 01:47:56 +0530

    Changing keys to secure string in powershell scrip

--- deployment/modules/prj/acl.ps1
+++ deployment/modules/prj/acl.ps1
@@ -1,7 +1,7 @@
 $storageAccountName = "${Env:saname}"
 $filesystemName = "${Env:fsname}"
 $userId = "${Env:user}"
-$keys
+[securestring]$keys
 $ctx = New-AzStorageContext -StorageAccountName 'dmw2dihadbsa01learning' -StorageAccountKey $keys
 $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $userId -Permission r--
 Update-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName -Acl $acl
