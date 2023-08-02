f8bbb5a89e0027c22d9b9f3806af817b67e45651
Prashant Singh <prashant.101386@gmail.com>
2023-08-03 01:53:55 +0530

    Created param in ps1 file

--- deployment/modules/prj/acl.ps1
+++ deployment/modules/prj/acl.ps1
@@ -1,8 +1,9 @@
+param ([string] $keys)
 $storageAccountName = "${Env:saname}"
 $filesystemName = "${Env:fsname}"
 $userId = "${Env:user}"
-[securestring]$keys
-$ctx = New-AzStorageContext -StorageAccountName 'dmw2dihadbsa01learning' -StorageAccountKey $keys
+$key = $keys
+$ctx = New-AzStorageContext -StorageAccountName 'dmw2dihadbsa01learning' -StorageAccountKey $key
 $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $userId -Permission r--
 Update-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName -Acl $acl
 $filesystem = Get-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName
