c5af4a83bd461f953b9d41b8f03dd906feb7bd99
Prashant Singh <prashant.101386@gmail.com>
2023-08-02 15:43:53 +0530

    Trying manually

--- deployment/modules/prj/acl.ps1
+++ deployment/modules/prj/acl.ps1
@@ -3,7 +3,7 @@ $StorageAccountKey = "${keys}"
 $filesystemName = "${Env:fsname}"
 $userId = "${Env:user}"
 
-$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $StorageAccountKey
+$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey 'DsAWseTsUqtVx+zrdm/AylHJkFxK+hjq7etYoBg5skV/N3C69nh8X4DT+qlWlu3xv5tByc2CvUjB+AStKYkVLA=='
 $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $userId -Permission r--
 Update-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName -Acl $acl
 $filesystem = Get-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName
