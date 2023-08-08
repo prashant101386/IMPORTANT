72914fce5f23615037f7891641c62ac90b92c136
Prashant Singh <prashant.101386@gmail.com>
2023-08-08 10:29:05 +0530

    Trial 7

--- deployment/modules/prj/acl.ps1
+++ deployment/modules/prj/acl.ps1
@@ -9,15 +9,15 @@ $storageAccountName = "${Env:saname}"
 # Filesystem name or container name passed as environment variable
 $filesystemName = "${Env:fsname}"
 # User
-$users = $users | ConvertFrom-Json
+#$users = $users | ConvertFrom-Json
 #$users = $users.TrimStart('[')
 Out-File -FilePath .\output.txt -InputObject $users
-#$usersArray = $users -split ','
+$usersArray = $users -split ','
 #Out-File -FilePath .\output1.txt -InputObject $usersArray
 #$user = "${Env:users}"
 $key = $keys
 $ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $key
-foreach ($usr in $users) {
+foreach ($usr in $usersArray) {
     $acl = (Get-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName).ACL
     $acl = set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityID $usr -Permission r-x -InputObject $acl
     Update-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName -Acl $acl
